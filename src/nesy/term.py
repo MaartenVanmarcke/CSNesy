cdb20a09ee1f671cd94ce5398a18ebfaa746fc63
Maarten Vanmarcke <maarten.vanmarcke@student.kuleuven.be>
2023-12-06 16:02:03 +0100

    Add extra functionality to the Term, Fact, Clause and Variable classes.
    Now, you can check if two instances have equal values and whether a term contains a variable and whether a clause contains a term or a variable.

--- src/nesy/term.py
+++ src/nesy/term.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-
+from collections import Counter
 
 class Term(namedtuple('Term', ['functor', 'arguments'])):
 
@@ -7,18 +7,44 @@ class Term(namedtuple('Term', ['functor', 'arguments'])):
         if len(self.arguments) == 0:
             return str(self.functor)
         return str(self.functor) + "(" + ",".join([str(a) for a in self.arguments]) + ")"
+    
+    def __eq__(self, __value: object) -> bool:
+        return (isinstance(__value, Term)
+                and self.functor == __value.functor
+                and Counter(self.arguments) == Counter(__value.arguments))
+    
+    def __contains__(self, __key: object) -> bool:
+        if isinstance(__key, Variable):
+            return __key in self.arguments
+        return super().__contains__(__key)
 
 
 class Variable(namedtuple('Variable', ['name'])):
 
     def __repr__(self):
         return str(self.name)
+    
+    def __eq__(self, __value: object) -> bool:
+        return (isinstance(__value, Variable)
+                and self.name == __value.name)
 
 
 class Clause(namedtuple('Clause', ['head', 'body'])):
 
     def __repr__(self):
         return str(self.head) + " :- " + ",".join([str(a) for a in self.body])
+    
+    def __eq__(self, __value: object) -> bool:
+        return (isinstance(__value, Clause)
+                and self.head == __value.head
+                and Counter(self.body) == Counter(__value.body))
+    
+    def __contains__(self, __key: object) -> bool:
+        if isinstance(__key, Variable):
+            return __key in self.head or any([__key in b for b in self.body])
+        if isinstance(__key, Term):
+            return __key == self.head or __key in self.body
+        return super().__contains__(__key)
 
 
 class Fact(namedtuple('Fact', ['term', 'weight'])):
@@ -28,3 +54,8 @@ class Fact(namedtuple('Fact', ['term', 'weight'])):
             return str(self.term)
         else:
             return str(self.weight) + "::" + str(self.term)
+        
+    def __eq__(self, __value: object) -> bool:
+        return (isinstance(__value, Fact)
+                and self.term == __value.term
+                and self.weight == __value.weight)
