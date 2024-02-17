fccad8a27d6cce5842cd30fb6a4b6795737a616a
r0800467 <eli.duval@student.kuleuven.be>
2024-02-17 17:37:00 +0100

    run documentation

--- example/run.py
+++ example/run.py
@@ -16,17 +16,25 @@ from pytorch_lightning.loggers import TensorBoardLogger
 
 logger = TensorBoardLogger(save_dir="logs/", name="model")  
 
+# STEP 1: define the desired n_digits and n_classes
 n_digits = 2
 n_classes = 2
+
+#STEP 2: construct the train, test-set and the neural_predicates
 task_train = AdditionTask(n=n_digits,n_classes=n_classes)
 task_test = AdditionTask(n=n_digits,n_classes=n_classes, train=False)
 
-print(task_train.program)
 neural_predicates = torch.nn.ModuleDict({"digit": MNISTEncoder(task_train.n_classes)})
 
+# STEP 3: define which caching you will use
 tree_caching = False
 use_nn_caching = False
-use_validation_set = False   # be able to see accuracy evolve over training BUT significantly slows everything down!
+
+#STEP 4: define if a validation set during training will be used to see the accuracy evolve over training
+# >> BUT if used, adds a lot of operations so makes the program a lot slower
+use_validation_set = False   
+
+#STEP 5: define the model + trainer +batch size
 model = NeSyModel(program=task_train.program,
                   logic_engine=ForwardChaining(tree_caching),
                   neural_predicates=neural_predicates,
@@ -37,6 +45,8 @@ if use_validation_set:
 else:
     trainer = pl.Trainer(max_epochs=1,logger=logger,log_every_n_steps=1)
 batch_size = 64
+
+#STEP 6: fit the model with train and validation data
 trainer.fit(model=model,
             train_dataloaders=task_train.dataloader(batch_size=batch_size),
             val_dataloaders=task_test.dataloader(batch_size=batch_size))
